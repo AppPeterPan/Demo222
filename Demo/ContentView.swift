b19c24ab0c07f77aae00528bfd01e623f5443f70
peterpan <apppeterpan@gmail.com>
2019-11-06 23:24:48 +0800

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by SHIH-YING PAN on 2019/11/6.
+//  Copyright © 2019 SHIH-YING PAN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
